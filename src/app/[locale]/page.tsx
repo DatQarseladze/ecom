fb34798d0fa19187122fc2c6fc656076c51ab663
Davit Karseladze <davit@Davits-MacBook-Pro.local>
2024-10-02 14:54:44 +0400

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,16 +6,76 @@ import {
   GoogleSignInButton,
   SignOutButton,
 } from "../../components/authButtons";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import { GoogleMap, Marker, useJsApiLoader, InfoWindow } from "@react-google-maps/api";
+
+// function highlightMarker(marker, highlight) {
+//   var color = "#FE7569";
+//   if (highlight) {
+//     color = "#0000FF";
+//   }
+//   marker.setImage(getIcon(color).image);
+// }
+
+const containerStyle = {
+  width: "100%",
+  height: "500px",
+};
+
+const center = {
+  lat: 41.72669982910156, // Default center point (e.g., New York)
+  lng: 44.72,
+};
+
+// Example office locations with some info
+const offices = [
+  { id: 1, position: { lat: 41.72562829536133, lng: 44.76167826503837 }, name: "", info: "This is Office 1" },
+  { id: 2, position: { lat: 41.726442, lng: 44.7549162 }, name: "", info: "This is Office 2" },
+  { id: 3, position: { lat: 41.7195921, lng: 44.7593418 }, name: "", info: "This is Office 3" },
+];
 
 const Page = () => {
   const { t } = useTranslation();
   const { data } = useSession();
+  const [selectedMarker, setSelectedMarker] = useState(null); // State for selected marker
 
-  useEffect(() => {});
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyCN9z63mq21nLn8zCiqs_TS5nRtZfw17uM", // Use your API key
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Container>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={13} // Zoom level to fit multiple locations
+      >
+        {offices.map((office) => (
+          <Marker
+            key={office.id}
+            position={office.position}
+            label={office.name ?? ""}
+            onClick={() => setSelectedMarker(office)} // Set selected marker on click
+          />
+        ))}
+
+        {selectedMarker && (
+          <InfoWindow
+            position={selectedMarker.position}
+            onCloseClick={() => setSelectedMarker(null)} // Clear selection on close
+          >
+            <div>
+              <h3>{selectedMarker.name}</h3>
+              <p>{selectedMarker.info}</p>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
       <GoogleSignInButton />
       <GithubSignInButton />
       {data && <SignOutButton />}
