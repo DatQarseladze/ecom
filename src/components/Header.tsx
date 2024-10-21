f8d11ed236a0fcc48b41b147cbe13ab956fbbf2e
Davit Karseladze <davit@Davits-MacBook-Pro.local>
2024-10-21 16:21:05 +0400

    header feat

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,62 @@
 "use client";
+import React, { useState } from "react";
 import Image from "next/image";
-import { useState } from "react";
-import { Menu } from "@headlessui/react";
 import { FaUser, FaHeart, FaShoppingCart } from "react-icons/fa";
 import { TextField, IconButton, Button, Switch } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+// import SearchIcon from "@mui/icons-material/Search";
 import Modal from "@mui/material/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../store/slices/themeSlice";
 import { RootState } from "../store";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import cartIcon from "../assets/images/cart.svg";
+import heartIcon from "../assets/images/heart.svg";
+import creditCardIcon from "../assets/images/credit-card.svg";
+import SearchIcon from "../assets/images/search.svg";
+import personIcon from "../assets/images/person.svg";
+import burgerIcon from "../assets/images/burger.svg";
 
-export default function Header() {
+const Navigation = () => {
+  return (
+    <nav className="container mx-auto px-4 py-2 flex items-center space-x-4 text-gray-700 font-medium mt-2">
+      <a
+        href="#"
+        className="hover:text-black bg-[#8255E3] text-[#fff] rounded-[8px] flex gap-[8px] px-[16px] h-[48px] items-center justify-center"
+      >
+        <Image
+          src={burgerIcon}
+          width={24}
+          height={24}
+          alt="burger"
+          className="self-center"
+        />
+        <div className="self-center text-[#fff]">კატეგორიები</div>
+      </a>
+      {[
+        "მედიკამენტები",
+        "სილამაზე და მოვლა",
+        "დედა და ბავშვი",
+        "ივენთები",
+        "ბლოგი",
+        "აქციები",
+        "AUTLET",
+        "ფილიალები",
+      ].map((item) => (
+        <a
+          key={item}
+          href="#"
+          className="relative group hover:text-[#8255E3] transition-colors duration-300"
+        >
+          <span className="relative z-10">{item}</span>
+          <span className="absolute left-0 right-0 bottom-[-10px] h-[1px] bg-[#E0D1FF] scale-x-0 transition-transform duration-300 group-hover:scale-x-100" />
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+const Header = () => {
   const theme = useSelector((state: RootState) => state.theme.value);
   const [search, setSearch] = useState("");
   const [open, setOpen] = useState(false);
@@ -29,147 +73,100 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-[#7787b5] shadow-sm">
-      <div className="container mx-auto px-4 py-2 flex items-center justify-between">
-        <div className="flex items-center space-x-4">
-          <Image
-            src="/impex.png"
-            alt="PSP"
-            height={50}
-            width={50}
-            className="rounded-[50%] cursor-pointer"
-            onClick={() => router.push("/")}
-          />
-
-          <div className="flex-grow max-w-md">
-            <TextField
-              fullWidth
-              variant="outlined"
-              placeholder="ძიება"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <IconButton>
-                    <SearchIcon />
-                  </IconButton>
-                ),
-              }}
-            />
-          </div>
-        </div>
-
-        <div className="flex items-center space-x-6">
-          {/* Navigation */}
-          <nav className="hidden lg:flex items-center space-x-4 ">
-            <div className="text-[#231444]">კატეგორიები</div>
-            <div className="text-[#231444]">მედიკამენტები</div>
-            <div className="text-[#231444]">კოსმეტიკა</div>
-            <div className="text-[#231444]">დედა & ბავშვი</div>
-            <Button href="#" variant="text" color="secondary">
-              BLOG
-            </Button>
-          </nav>
-
-          {/* Icons */}
-          <div className="flex items-center space-x-4">
-            <Switch checked={theme === "dark"} onChange={handleTheme} />
-            <IconButton color="inherit" className="text-gray-500">
-              <FaHeart />
-            </IconButton>
-            {user?.data ? (
+    <header className="bg-[#ffffff] shadow-sm">
+      {/* <div className="container mx-auto px-4 py-2"> */}
+      <div>
+        <div className="flex items-center justify-between pb-[12px] border-b-[1px] border-[#1B1D200F]">
+          <div className="container mx-auto px-4 py-2 flex items-center justify-between">
+            <div className="flex items-center space-x-8">
               <Image
-                src={user?.data?.user?.image || ""}
-                width={50}
-                alt="user image"
-                onClick={redirectToProfile}
-                height={50}
-                className="cursor-pointer rounded-[50%]"
+                src="/logo.svg"
+                alt="PSP"
+                height={56}
+                width={170}
+                className="cursor-pointer"
+                onClick={() => router.push("/")}
               />
-            ) : (
-              <IconButton
-                onClick={() => setOpen(true)}
-                color="inherit"
-                className="text-gray-500"
-              >
-                <FaUser />
-              </IconButton>
-            )}
-            <IconButton color="inherit" className="text-gray-500">
-              <FaShoppingCart />
-            </IconButton>
-          </div>
-          <div className="lg:hidden">
-            <Menu>
-              <Menu.Button className="p-2 text-gray-500 focus:outline-none">
-                ☰
-              </Menu.Button>
-              <Menu.Items className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 shadow-lg">
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={`block px-4 py-2 ${active ? "bg-gray-100" : "bg-gray-500"}`}
-                    >
-                      კატეგორიები
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={`block px-4 py-2 ${active ? "bg-gray-100" : ""}`}
-                    >
-                      მედიკამენტები
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={`block px-4 py-2 ${active ? "bg-gray-100" : ""}`}
-                    >
-                      კოსმეტიკა
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={`block px-4 py-2 ${active ? "bg-gray-100" : ""}`}
-                    >
-                      დედა & ბავშვი
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={`block px-4 py-2 ${active ? "bg-gray-100" : ""} text-pink-500`}
-                    >
-                      BLOG
-                    </a>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
-            </Menu>
+            </div>
+
+            <div className="flex items-center space-x-4">
+              <div className="flex-grow w-[568px] max-w-md">
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  placeholder="ჩაწერე საძიებო სიტყვა"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  sx={{
+                    height: 56,
+                    borderRadius: "8px", // Rounded corners
+                    border: "1px solid #1B1D201A", // Custom border
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        borderColor: "transparent", // Hide the default border
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "transparent", // Hide the border on hover
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "transparent", // Hide the border when focused
+                      },
+                    },
+                    "& .MuiInputBase-root": {
+                      height: "100%",
+                    },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <Image
+                        src={SearchIcon}
+                        alt="search"
+                        width={40}
+                        height={40}
+                        className="cursor-pointer"
+                      />
+                    ),
+                  }}
+                />
+              </div>
+              <div className="cursor-pointer w-[176px] h-[56px] rounded-[8px] border-[1px] border-[#1B1D201A] flex items-center justify-center">
+                <Image
+                  src={creditCardIcon}
+                  alt="cart"
+                  className="mr-[8px]"
+                  width={24}
+                  height={24}
+                />
+                <span>ზე ბარათი</span>
+              </div>
+              <div className="cursor-pointer w-[56px] h-[56px] rounded-[8px] border-[1px] border-[#1B1D201A] flex items-center justify-center">
+                <Image src={cartIcon} alt="cart" width={24} height={24} />
+              </div>
+              <div className="cursor-pointer w-[56px] h-[56px] rounded-[8px] border-[1px] border-[#1B1D201A] flex items-center justify-center">
+                <Image src={heartIcon} alt="heart" width={24} height={24} />
+              </div>
+              <div className="cursor-pointer w-[56px] h-[56px] rounded-[8px] border-[1px] border-[#1B1D201A] flex items-center justify-center">
+                <Image src={personIcon} alt="person" width={24} height={24} />
+              </div>
+            </div>
           </div>
         </div>
+
+        <Navigation />
+
+        {/* <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <div className="w-[400px] h-[400px] transform bg-[#fff] translate-x-[-50%] translate-y-[-50%] absolute rounded-[12px] top-[50%] left-[50%]">
+            <div className="text-black text-center">Login Modal</div>
+          </div>
+        </Modal> */}
       </div>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <div className="w-[400px] h-[400px] transform bg-[#fff] translate-x-[-50%] translate-y-[-50%] absolute rounded-[12px]  top-[50%] left-[50%] ">
-          <div className="text-black text-center">Login Modal</div>
-        </div>
-      </Modal>
     </header>
   );
-}
+};
+
+export default Header;
