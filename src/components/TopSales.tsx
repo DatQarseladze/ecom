1916b54b75c60014ed0919c117e94410f94f7673
Davit Karseladze <davit@Davits-MacBook-Pro.local>
2024-11-06 10:25:36 +0400

    remove reusable components

--- src/components/TopSales.tsx
+++ src/components/TopSales.tsx
@@ -1,59 +1,23 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
 import Image from "next/image";
-import productImg1 from "../assets/images/product.png";
-import productImg2 from "../assets/images/product.png";
-import productImg3 from "../assets/images/product1.png";
-import productImg4 from "../assets/images/product2.png";
 import caretRight from "../assets/images/caret-right.svg";
 import caretLeft from "../assets/images/caret-left.svg";
-import heartIcon from "../assets/images/heart.svg";
-import leafIcon from "../assets/images/leaf.svg";
-import starIcon from "../assets/images/star.svg";
-import clockIcon from "../assets/images/clock.svg";
-import organIcon from "../assets/images/organ.svg";
-import ratingIcon from "../assets/images/rating.svg";
-import bestsellerIcon from "../assets/images/bestseller.svg";
-import dotIcon from "../assets/images/dot.svg";
-import voltIcon from "../assets/images/volt.svg";
-import truckIcon from "../assets/images/truck.svg";
-import wastingTimeIcon from "../assets/images/wasting-time.svg";
 import filledHeart from "../assets/images/filled-heart.svg";
-
-const products = [
-  {
-    id: 1,
-    discount: "-42%",
-    title: "La Roche-Posay - ლა როშ-პოზე სკრაბი მგრძნობიარე კანისთვის 50 მლ",
-    price: "₾58.48",
-    originalPrice: "₾97.47",
-    img: productImg1,
-  },
-  {
-    id: 2,
-    discount: "-42%",
-    title: "La Roche-Posay - ლა როშ-პოზე სკრაბი მგრძნობიარე კანისთვის 50 მლ",
-    price: "₾58.48",
-    originalPrice: "₾97.47",
-    img: productImg2,
-  },
-  {
-    id: 3,
-    discount: "-42%",
-    title: "La Roche-Posay - ლა როშ-პოზე სკრაბი მგრძნობიარე კანისთვის 50 მლ",
-    price: "₾58.48",
-    originalPrice: "₾97.47",
-    img: productImg3,
-  },
-  {
-    id: 4,
-    discount: "-42%",
-    title: "La Roche-Posay - ლა როშ-პოზე სკრაბი მგრძნობიარე კანისთვის 50 მლ",
-    price: "₾58.48",
-    originalPrice: "₾97.47",
-    img: productImg4,
-  },
-];
+import {
+  products,
+  heartIcon,
+  leafIcon,
+  starIcon,
+  clockIcon,
+  organIcon,
+  ratingIcon,
+  bestsellerIcon,
+  dotIcon,
+  voltIcon,
+  truckIcon,
+  wastingTimeIcon,
+} from "./constants";
 
 const ProductList = () => {
   return (
@@ -98,25 +62,79 @@ const ProductList = () => {
                 </div>
                 <div className="flex space-x-2">
                   {index === 0 ? (
-                    <OrganButton />
+                    <HoverButton
+                      iconSrc={organIcon}
+                      bgColor={"#01B21E"}
+                      borderColor={"#A3E6CD"}
+                      label={"ორგანული"}
+                    />
                   ) : index === 1 ? (
-                    <WastingTimeButton />
+                    <HoverButton
+                      iconSrc={wastingTimeIcon}
+                      bgColor={"#D14343"}
+                      borderColor={"#EE9191"}
+                      label={"შეზღუდული მარაგი"}
+                    />
                   ) : index === 2 ? (
-                    <DotButton />
+                    <HoverButton
+                      iconSrc={dotIcon}
+                      bgColor={"#219653"}
+                      borderColor={"#A3E6CD"}
+                      label={"მხოლოდ ონლაინ"}
+                    />
                   ) : (
-                    <StarButton />
+                    <HoverButton
+                      iconSrc={starIcon}
+                      bgColor={"#FFD000"}
+                      borderColor={"#ffd000"}
+                      label={"ექსკლუზივი"}
+                    />
                   )}
                   {index === 0 ? (
-                    <RatingButton />
+                    <HoverButton
+                      iconSrc={ratingIcon}
+                      bgColor={"#F18D13"}
+                      borderColor={"#FAA53D"}
+                      label={"ყველაზე რეიტინგული"}
+                    />
                   ) : index === 1 ? (
-                    <BestsellerButton />
+                    <HoverButton
+                      iconSrc={bestsellerIcon}
+                      bgColor={"#ED55C2"}
+                      borderColor={"#F499DA"}
+                      label={"ბესტსელერი"}
+                    />
                   ) : index === 2 ? (
-                    <VoltButton />
+                    <HoverButton
+                      iconSrc={voltIcon}
+                      bgColor={"#9A6DF9"}
+                      borderColor={"#CBB2FF"}
+                      label={"სიახლე"}
+                    />
                   ) : (
-                    <VeganButton />
+                    <HoverButton
+                      iconSrc={leafIcon}
+                      bgColor={"#82B536"}
+                      borderColor={"#94C748"}
+                      label={"ვეგანური"}
+                    />
                   )}
-                  {index === 3 && <TruckButton />}
-                  {index === 2 && <ClockButton />}
+                  {index === 3 && (
+                    <HoverButton
+                      iconSrc={truckIcon}
+                      bgColor={"#3366FF"}
+                      borderColor={"#9DB5FF"}
+                      label={"უფასო მიწოდება"}
+                    />
+                  )}
+                  {index === 2 && (
+                    <HoverButton
+                      iconSrc={clockIcon}
+                      bgColor={"#FFD000"}
+                      borderColor={"#ffd000"}
+                      label={"21 ოქტ - 30 ოქტ"}
+                    />
+                  )}
                 </div>
               </div>
 
@@ -131,7 +149,6 @@ const ProductList = () => {
                 />
               </div>
               <div className="flex flex-col">
-                {/* Product Title with Ellipsis */}
                 <h3 className="text-[20px] leading-[28px] text-[#101840] font-bold overflow-hidden text-ellipsis break-all line-clamp-2">
                   {product.title}
                 </h3>
@@ -187,17 +204,17 @@ const ProductList = () => {
   );
 };
 
-const VoltButton = () => {
+const HoverButton = ({ iconSrc, bgColor, borderColor, label }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div
-      className="flex items-center border-[1px] bg-[#9A6DF9] border-solid border-[#CBB2FF] h-[32px] px-[8px] rounded-[8px] transition-all duration-500 ease-in-out"
+      className={`flex items-center border-[1px] bg-[${bgColor}] border-solid border-[${borderColor}] h-[32px] px-[8px] rounded-[8px] transition-all duration-500 ease-in-out`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="flex items-center justify-center">
-        <Image src={voltIcon} width={16} height={16} alt="star icon" />
+        <Image src={iconSrc} width={16} height={16} alt={`${label} icon`} />
       </div>
       <div
         className={`text-[16px] leading-[28px] text-white transition-all duration-500 ease-in-out ${
@@ -208,276 +225,11 @@ const VoltButton = () => {
         style={{
           visibility: isHovered ? "visible" : "hidden",
           marginLeft: isHovered ? "8px" : "0",
-          overflow: "hidden", // Prevent overflow
+          overflow: "hidden",
         }}
       >
-        <span>სიახლე</span>
+        <span>{label}</span>
       </div>
-    </div>
-  );
-};
-
-const BestsellerButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className="flex items-center border-[1px] bg-[#ED55C2] border-solid border-[#F499DA] h-[32px] px-[8px] rounded-[8px] transition-all duration-500 ease-in-out"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={bestsellerIcon} width={16} height={16} alt="star icon" />
-      </div>
-      <div
-        className={`text-[16px] leading-[28px] text-white transition-all duration-500 ease-in-out ${
-          isHovered
-            ? "opacity-100 max-w-xs transform scale-100"
-            : "opacity-0 max-w-0 transform scale-0"
-        }`}
-        style={{
-          visibility: isHovered ? "visible" : "hidden",
-          marginLeft: isHovered ? "8px" : "0",
-          overflow: "hidden", // Prevent overflow
-        }}
-      >
-        <span>ბესტსელერი</span>
-      </div>
-    </div>
-  );
-};
-
-const WastingTimeButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className="flex items-center border-[1px] bg-[#D14343] border-solid border-[#EE9191] h-[32px] px-[8px] rounded-[8px] transition-all duration-500 ease-in-out"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={wastingTimeIcon} width={16} height={16} alt="star icon" />
-      </div>
-      <div
-        className={`text-[16px] leading-[28px] text-white transition-all duration-500 ease-in-out ${
-          isHovered
-            ? "opacity-100 max-w-xs transform scale-100"
-            : "opacity-0 max-w-0 transform scale-0"
-        }`}
-        style={{
-          visibility: isHovered ? "visible" : "hidden",
-          marginLeft: isHovered ? "8px" : "0",
-          overflow: "hidden", // Prevent overflow
-        }}
-      >
-        <span>შეზღუდული მარაგი</span>
-      </div>
-    </div>
-  );
-};
-
-const TruckButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className="flex items-center border-[1px] bg-[#3366FF] border-solid border-[#9DB5FF] h-[32px] px-[8px] rounded-[8px] transition-all duration-500 ease-in-out"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={truckIcon} width={16} height={16} alt="star icon" />
-      </div>
-      <div
-        className={`text-[16px] leading-[28px] text-white transition-all duration-500 ease-in-out ${
-          isHovered
-            ? "opacity-100 max-w-xs transform scale-100"
-            : "opacity-0 max-w-0 transform scale-0"
-        }`}
-        style={{
-          visibility: isHovered ? "visible" : "hidden",
-          marginLeft: isHovered ? "8px" : "0",
-          overflow: "hidden", // Prevent overflow
-        }}
-      >
-        <span>უფასო მიწოდება</span>
-      </div>
-    </div>
-  );
-};
-
-const DotButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className="flex items-center border-[1px] bg-[#219653] border-solid border-[#A3E6CD] h-[32px] px-[8px] rounded-[8px] transition-all duration-500 ease-in-out"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={dotIcon} width={16} height={16} alt="star icon" />
-      </div>
-      <div
-        className={`text-[16px] leading-[28px] text-white transition-all duration-500 ease-in-out ${
-          isHovered
-            ? "opacity-100 max-w-xs transform scale-100"
-            : "opacity-0 max-w-0 transform scale-0"
-        }`}
-        style={{
-          visibility: isHovered ? "visible" : "hidden",
-          marginLeft: isHovered ? "8px" : "0",
-          overflow: "hidden", // Prevent overflow
-        }}
-      >
-        <span>მხოლოდ ონლაინ</span>
-      </div>
-    </div>
-  );
-};
-const RatingButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className="flex items-center border-[1px] bg-[#F18D13] border-solid border-[#FAA53D] h-[32px] px-[8px] rounded-[8px] transition-all duration-500 ease-in-out"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={ratingIcon} width={16} height={16} alt="star icon" />
-      </div>
-      <div
-        className={`text-[16px] leading-[28px] text-white transition-all duration-500 ease-in-out ${
-          isHovered
-            ? "opacity-100 max-w-xs transform scale-100"
-            : "opacity-0 max-w-0 transform scale-0"
-        }`}
-        style={{
-          visibility: isHovered ? "visible" : "hidden",
-          marginLeft: isHovered ? "8px" : "0",
-          overflow: "hidden", // Prevent overflow
-        }}
-      >
-        <span>ყველას რეიტინგული</span>
-      </div>
-    </div>
-  );
-};
-
-const OrganButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className={`flex items-center border-[1px] bg-[#01B21E] border-solid border-[#A3E6CD] h-[32px] px-[8px] transition-all duration-500 ease-in-out rounded-[8px] bg-[#FFD000] overflow-hidden ${
-        isHovered ? "max-w-[150px]" : "max-w-[32px]"
-      }
-          `}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={organIcon} width={16} height={16} alt="star icon" />
-      </div>
-      {isHovered && (
-        <div
-          className={`ml-[4px] text-[16px] leading-[28px] text-white transition-transform duration-500 ${
-            isHovered ? "translate-x-0" : "translate-x-[-100%]"
-          }`}
-        >
-          ორგანული
-        </div>
-      )}
-    </div>
-  );
-};
-
-const ClockButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className={`flex items-center border-[1px] border-solid border-[#ffd000] h-[32px] px-[8px] transition-all duration-500 ease-in-out rounded-[8px] bg-[#FFD000] overflow-hidden ${
-        isHovered
-          ? "max-w-[150px] bg-[#E4AA16] border-[#e4aa16]"
-          : "max-w-[32px]"
-      }
-          `}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={clockIcon} width={16} height={16} alt="star icon" />
-      </div>
-      {isHovered && (
-        <div
-          className={`ml-[4px] text-[16px] leading-[28px] text-white transition-transform duration-500 ${
-            isHovered ? "translate-x-0" : "translate-x-[-100%]"
-          }`}
-        >
-          21 ოქტ - 30 ოქტ
-        </div>
-      )}
-    </div>
-  );
-};
-
-const StarButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className={`flex items-center border-[1px] border-solid border-[#ffd000] h-[32px] px-[8px] transition-all duration-500 ease-in-out rounded-[8px] bg-[#FFD000] overflow-hidden ${
-        isHovered
-          ? "max-w-[150px] bg-[#E4AA16] border-[#e4aa16]"
-          : "max-w-[32px]"
-      }
-          `}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={starIcon} width={16} height={16} alt="star icon" />
-      </div>
-      {isHovered && (
-        <div
-          className={`ml-[4px] text-[16px] leading-[28px] text-white transition-transform duration-500 ${
-            isHovered ? "translate-x-0" : "translate-x-[-100%]"
-          }`}
-        >
-          ექსკლუზივი
-        </div>
-      )}
-    </div>
-  );
-};
-
-const VeganButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className={`flex items-center border-[1px] border-solid border-[#94C748] h-[32px] px-[8px] transition-all duration-500 ease-in-out rounded-[8px] bg-[#82B536] overflow-hidden ${
-        isHovered ? "max-w-[150px]" : "max-w-[32px]"
-      }
-        `}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-center">
-        <Image src={leafIcon} width={16} height={16} alt="leaf icon" />
-      </div>
-      {isHovered && (
-        <div
-          className={`ml-[4px] text-[16px] leading-[28px] text-white transition-transform duration-500 ${
-            isHovered ? "translate-x-0" : "translate-x-[-100%]"
-          }`}
-        >
-          ვეგანური
-        </div>
-      )}
     </div>
   );
 };
---
+++ src/components/constants.ts
@@ -0,0 +1,65 @@
+import productImg1 from "../assets/images/product.png";
+import productImg2 from "../assets/images/product.png";
+import productImg3 from "../assets/images/product1.png";
+import productImg4 from "../assets/images/product2.png";
+
+import heartIcon from "../assets/images/heart.svg";
+import leafIcon from "../assets/images/leaf.svg";
+import starIcon from "../assets/images/star.svg";
+import clockIcon from "../assets/images/clock.svg";
+import organIcon from "../assets/images/organ.svg";
+import ratingIcon from "../assets/images/rating.svg";
+import bestsellerIcon from "../assets/images/bestseller.svg";
+import dotIcon from "../assets/images/dot.svg";
+import voltIcon from "../assets/images/volt.svg";
+import truckIcon from "../assets/images/truck.svg";
+import wastingTimeIcon from "../assets/images/wasting-time.svg";
+
+export {
+  heartIcon,
+  leafIcon,
+  starIcon,
+  clockIcon,
+  organIcon,
+  ratingIcon,
+  bestsellerIcon,
+  dotIcon,
+  voltIcon,
+  truckIcon,
+  wastingTimeIcon,
+};
+
+export const products = [
+  {
+    id: 1,
+    discount: "-42%",
+    title: "La Roche-Posay - ლა როშ-პოზე სკრაბი მგრძნობიარე კანისთვის 50 მლ",
+    price: "₾58.48",
+    originalPrice: "₾97.47",
+    img: productImg1,
+  },
+  {
+    id: 2,
+    discount: "-42%",
+    title: "La Roche-Posay - ლა როშ-პოზე სკრაბი მგრძნობიარე კანისთვის 50 მლ",
+    price: "₾58.48",
+    originalPrice: "₾97.47",
+    img: productImg2,
+  },
+  {
+    id: 3,
+    discount: "-42%",
+    title: "La Roche-Posay - ლა როშ-პოზე სკრაბი მგრძნობიარე კანისთვის 50 მლ",
+    price: "₾58.48",
+    originalPrice: "₾97.47",
+    img: productImg3,
+  },
+  {
+    id: 4,
+    discount: "-42%",
+    title: "La Roche-Posay - ლა როშ-პოზე სკრაბი მგრძნობიარე კანისთვის 50 მლ",
+    price: "₾58.48",
+    originalPrice: "₾97.47",
+    img: productImg4,
+  },
+];
