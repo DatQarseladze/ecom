da054457217ae203fd89aef502384220ad63d2a3
Davit Karseladze <davit@Davits-MacBook-Pro.local>
2024-11-13 17:29:07 +0400

    update

--- src/components/ReusableBadge.tsx
+++ src/components/ReusableBadge.tsx
@@ -13,7 +13,7 @@ const ReusableBadge = ({
 
   return (
     <div
-      className="flex relative z-40 items-center border h-[32px] px-[7px] rounded-[8px] border-[1px] border-solid transition-all duration-500 ease-in-out"
+      className="flex relative z-40 items-center border h-[32px] px-[7px] pr-[0] rounded-[8px] border-[1px] border-solid transition-all duration-500 ease-in-out"
       style={{
         backgroundColor: isHovered ? hoverBgColor || bgColor : bgColor,
         borderColor: isHovered ? hoverBorderColor || borderColor : borderColor,
@@ -26,13 +26,14 @@ const ReusableBadge = ({
       </div>
       <p
         className={`leading-[28px] text-white text-[14px] transition-all duration-500 ease-in-out ${
-          isHovered ? "opacity-100 scale-100" : "opacity-0"
+          isHovered ? "" : "opacity-0"
         }`}
         style={{
           overflow: "hidden",
-          padding: isHovered ? "8px" : "0",
+          // padding: isHovered ? "8px" : "0",
+          marginLeft: '8px',
+          marginRight: isHovered ? "8px" : "0",
           maxWidth: isHovered ? "260px" : "0px",
-          fontWeight: 400,
         }}
       >
         {label}
