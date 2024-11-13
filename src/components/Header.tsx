36bc34d87d91d1d96e55c70b41aa9277edea27ab
Davit Karseladze <davit@Davits-MacBook-Pro.local>
2024-11-13 10:58:54 +0400

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ const Navigation = () => {
         <React.Fragment key={item}>
           <a
             href="#"
-            className={`relative text-[18px] web-xl:text-[16px] hover:bg-[#1B1D2008] rounded-[2px] leading-[24px] py-[12px] px-[8px] web-xl:py-[6px] group transition-colors duration-300 ${
+            className={`relative text-[18px] web-xl:text-[16px] font-[400] hover:bg-[#1B1D2008] rounded-[2px] leading-[24px] py-[12px] px-[8px] web-xl:py-[6px] group transition-colors duration-300 ${
               activeTab === item
                 ? "text-[#8255E3] border-b-[2px] border-b-[#dfd2ff]"
                 : "text-gray-700"
