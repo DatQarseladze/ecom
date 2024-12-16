0eafd3f53558d47ce04c56a026d65d8994df19b6
Davit Karseladze <davit@Davits-MacBook-Pro.local>
2024-12-16 12:27:08 +0400

    fix build

--- src/app/[locale]/(routes)/product/page.tsx
+++ src/app/[locale]/(routes)/product/page.tsx
@@ -1,51 +1,8 @@
-'use client'
-import React, { useState } from "react";
-import Image from "next/image";
-import caretRight from "../assets/images/caret-right.svg";
-import caretLeft from "../assets/images/caret-left.svg";
-import { useRouter } from "next/navigation";
-import { products } from "@/src/components/constants";
-
+"use client";
 
 const ProductList = () => {
-  const [counts, setCounts] = useState({});
-  const route = useRouter();
-
-  const handleCountChange = (index, change) => {
-    setCounts((prevCounts) => {
-      const currentCount = prevCounts[index] || 0;
-      const newCount = Math.max(0, Math.min(10, currentCount + change));
-      return { ...prevCounts, [index]: newCount };
-    });
-  };
   return (
-    <div className="2xl:mx-[168px] max-2xl:mx-[168px] bg-[#FBFBFD] px-auto pt-[64px] pb-[80px]">
-      <div className="py-[4px] flex items-center justify-between mb-[48px]">
-        <h2 className="text-[24px] leading-[32px] text-[#474D66] font-bold">
-          ტოპ გაყიდვადი
-        </h2>
-        <div className="flex items-center gap-[16px]">
-          <h3 className="cursor-pointer text-[16px] text-[#101840] leading-[24px] border-b-[1px] border-transparent hover:border-[#474D66] hover:text-[#474D66]">
-            ყველას ნახვა
-          </h3>
-
-          <div className="flex items-center gap-[4px]">
-            <div className="cursor-pointer border-[1px] p-[9px] border-[#1B1D2008] flex items-center justify-center rounded-[8px]">
-              <Image src={caretLeft} width={20} height={20} alt="caret-left" />
-            </div>
-            <div className="cursor-pointer hover:bg-[#1B1D2008] border-[1px] p-[9px] border-[#1B1D201A] flex items-center justify-center rounded-[8px]">
-              <Image
-                src={caretRight}
-                width={20}
-                height={20}
-                alt="caret-right"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
+    <div className="2xl:mx-[168px] max-2xl:mx-[168px] bg-[#FBFBFD] px-auto pt-[64px] pb-[80px]">Test</div>
   );
 };
 
