fcbd702528283a81c2e2b28a755e7ea08fe8c139
Davit Karseladze <davit@Davits-MacBook-Pro.local>
2024-12-16 16:04:07 +0400

    IWA_274

--- src/app/[locale]/(routes)/product/[id]/page.tsx
+++ src/app/[locale]/(routes)/product/[id]/page.tsx
@@ -262,7 +262,7 @@ const ProductList = () => {
                 </div>
               </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[8px]">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[8px] pb-[40px]">
               {weekProducts
                 ?.filter((product, index) => index <= 3)
                 .map((product, index) => (
